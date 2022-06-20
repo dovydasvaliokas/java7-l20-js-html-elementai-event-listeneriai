32661255d1317c5a320de8e4cb3359c7b6f3e85b
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-06-20 15:15:18 +0300

    Baigtas pavyzdys su dinamiskai pridedamu ivedimo laukeliu ir skaiciu masyvo nuskaitymu is ivedimo langeliu (input) masyvo

--- pvz4-masyvo-nuskaitymas-kuriant-ivedimo-laukelius/script.js
+++ pvz4-masyvo-nuskaitymas-kuriant-ivedimo-laukelius/script.js
@@ -1,3 +1,36 @@
 window.onload = function() {
+    let pliusoMygtukas = document.getElementById("pliusoMygtukas");
+    let skaiciavimoMygtukas = document.getElementById("skaiciavimoMygtukas");
+
+    pliusoMygtukas.onclick = function() {
+        idetiIvedimoLaukeli(pliusoMygtukas);
+    }
+
+    skaiciavimoMygtukas.onclick = function() {
+        const langeliai = document.getElementsByClassName("masyvas");
+        const skaiciai = nuskaitytiSkaicius(langeliai);
+
+        console.log(skaiciai);
+    }
     
+
+}
+
+
+function idetiIvedimoLaukeli(elementasPriesKuriIdeti) {
+    let ivedimoLaukelis = document.createElement("input");
+    ivedimoLaukelis.setAttribute("type", "number");
+    ivedimoLaukelis.classList.add("masyvas");
+    elementasPriesKuriIdeti.before(ivedimoLaukelis);
+}
+
+
+
+function nuskaitytiSkaicius(langeliai) {
+    const skaiciai = [];
+    for (let i = 0; i < langeliai.length; i++) {
+        skaiciai.push(langeliai[i].valueAsNumber);
+        
+    }
+    return skaiciai;
 }
