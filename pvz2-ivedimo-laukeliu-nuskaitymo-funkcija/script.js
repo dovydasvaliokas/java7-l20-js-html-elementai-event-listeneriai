d7e794fcf63de7dbb1b583333e3c8e44db6f833a
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-06-20 14:33:54 +0300

    Baigtas antras pavyzdys su nuskaitymo funkcija

--- pvz2-ivedimo-laukeliu-nuskaitymo-funkcija/script.js
+++ pvz2-ivedimo-laukeliu-nuskaitymo-funkcija/script.js
@@ -1,3 +1,25 @@
 window.onload = function() {
+
+    let mygtukas = document.getElementById("mygtukas");
+
+    mygtukas.onclick = function() {
+        let a = nuskaitoIvedimoLaukeli("a");
+        let b = nuskaitoIvedimoLaukeli("b");
+        let c = nuskaitoIvedimoLaukeli("c");
+        let d = nuskaitoIvedimoLaukeli("d");
+
+        let pastraipa = document.getElementById("rezultatas");
+        pastraipa.innerHTML = "Keturkampio perimetras yra: " + keturkampioPerimetras(a, b, c, d);
+    }
     
+}
+
+
+function nuskaitoIvedimoLaukeli(id) {
+    let ivedimoLaukelis = document.getElementById(id);
+    return ivedimoLaukelis.valueAsNumber;
+}
+
+function keturkampioPerimetras(a, b, c, d) {
+    return a + b + c + d;
 }
