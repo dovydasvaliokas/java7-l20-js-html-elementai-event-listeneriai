122df163e01e9b43be27b1f5d4566795466a8cf6
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-06-20 14:12:17 +0300

    Pirmas pavyzdys su ivedimo laukeliais, mygtuku, onclicku, pastraipa, innerHTML

--- pvz1-ivedimo-laukeliai-mygtukas/script.js
+++ pvz1-ivedimo-laukeliai-mygtukas/script.js
@@ -1,3 +1,29 @@
 window.onload = function() {
-    
+    let ilgioIvedimoLaukelis = document.getElementById("ilgis");
+    console.log(ilgioIvedimoLaukelis);
+
+    let plocioIvedimoLaukelis = document.getElementById("plotis");
+
+    let mygtukas = document.getElementById("skaiciavimoMygtukas");
+    console.log(mygtukas);
+
+    let pastraipa = document.getElementById("rezultatoPastraipa");
+
+    mygtukas.onclick = function() {
+        let ilgis = ilgioIvedimoLaukelis.valueAsNumber;
+        console.log(ilgis);
+
+        let plotis = plocioIvedimoLaukelis.valueAsNumber;
+        console.log(plotis);
+
+        console.log(staciakampioPlotas(ilgis, plotis));
+
+        pastraipa.innerHTML = "Stačiakampio plotas yra: " + staciakampioPlotas(ilgis, plotis);
+    }
+}
+
+
+
+function staciakampioPlotas(a, b) {
+    return a * b;
 }
